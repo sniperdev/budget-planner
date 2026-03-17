579a7e992787715cc980b067efa2e36d9c74dbfd
sniperdev <igorspychalaa@gmail.com>
2026-03-17 22:52:14 +0100

    feat(api): initialize Prisma setup and user authentication module

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,12 +1,12 @@
 import { Module } from '@nestjs/common';
 
-import { LinksModule } from './links/links.module';
-
 import { AppService } from './app.service';
 import { AppController } from './app.controller';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [LinksModule],
+  imports: [AuthModule, UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
